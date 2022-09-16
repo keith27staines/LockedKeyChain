f5f13ec2d248efd779cb7b5ccb46c1fca732477e
generic-keith <keith.staines.ext@bbc.co.uk>
2022-09-16 15:10:38 +0100

    Initial Commit

---
+++ LockedKeyChain/LockedKeyChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockedKeyChainApp.swift
+//  LockedKeyChain
+//
+//  Created by Keith Staines on 16/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LockedKeyChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
